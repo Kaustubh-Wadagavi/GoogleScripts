1a26c275bf7a2290b062f0fce8cb987f5e6aaffd
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2021-05-31 16:12:08 +0530

    Changed functions

--- GetOSUsers.js
+++ GetOSUsers.js
@@ -1,8 +1,8 @@
-var AUTH_URL = 'https://demo.openspecimen.org/rest/ng/sessions';
-var API_URL = 'https://demo.openspecimen.org/rest/ng/users';
+var AUTH_URL = 'https://test.openspecimen.org/rest/ng/sessions';
+var API_URL = 'https://test.openspecimen.org/rest/ng/users';
 
 var data = {
-    "loginName": "kaustubh@krishagni.com",
+    "loginName": "kaustubh",
     "password": "Login@123",
     "domainName": "openspecimen"
 } 
@@ -11,6 +11,7 @@ function MainFunction() {
   var options = {method : "post", contentType:"application/json", payload : JSON.stringify(data),"muteHttpExceptions":true};
   var response = UrlFetchApp.fetch(AUTH_URL, options);
   Logger.log(response.getContentText());  
+  getAllUsers();
 }
 
 function getAllUsers() {
